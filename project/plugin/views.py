2355d25780892ff82db7aa9b1db82d5438c3d61d
Jayakrishnan Ashok <jayakrishnanashok@gmail.com>
2018-03-14 14:19:03 +0530

    Add files via upload

--- project/plugin/views.py
+++ project/plugin/views.py
@@ -15,6 +15,158 @@ import re
 import numpy as np
 import pandas as pd
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+import urllib.request
+
+from bs4.element import Comment
+from statistics import mode
+import nltk
+import random
+
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+
+from nltk.tokenize import word_tokenize
+from nltk.classify import ClassifierI
+import codecs   ##used beacause our dataset is in latin
+
+def convert_to_tokens(input_url):
+	tokens_red_slash=str(input_url.encode('utf-8')).split('/')
+	all_tokens=[]
+	for i in tokens_red_slash:
+		tokens=str(i).split('-')
+		tokens_dot=[]
+		for j in range(0,len(tokens)):
+			temp=str(tokens[j]).split('.')
+			tokens_dot=tokens_dot+temp
+		all_tokens=all_tokens+tokens+tokens_dot
+	all_tokens=list(set(all_tokens))
+	if 'com'in all_tokens:
+		all_tokens.remove('com')
+	return all_tokens
+ 
+
+###############################################
+
+
+####################################. run
+def Visible(element):
+  if element.parent.name in ['style' , 'script' ,'[document]' , 'head' ,'title']:
+    return False
+  if isinstance(element,Comment):
+    return False
+
+  return True
+
+def text_from_html(body):
+  soup=BeautifulSoup(body,'html.parser')
+  texts=soup.findAll(text=True)
+  result=filter(Visible,texts)
+  return u" ".join(t.strip() for t in result)
+  
+####################################. run
+
+
+
+## it is a function for calculating votes so as to check which classifier we are using 
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+##implementation of codecs 
+with codecs.open("/Users/jk/Documents/Major/plugin_Test/positive.txt", "r", "latin-1") as inputfile:
+    short_pos=inputfile.read()
+with codecs.open("/Users/jk/Documents/Major/plugin_Test/negative.txt", "r", "latin-1") as inputfile:
+    short_neg=inputfile.read()
+
+all_words = []
+documents = []
+
+
+#  here j is adjective , r is adverb, and v is verb but we are removing all the adverb and verb here
+#allowed_word_types = ["J","R","V"]
+allowed_word_types = ["J"]
+
+for p in short_pos.split('\n'):
+    documents.append( (p, "0") )
+    words = word_tokenize(p)
+    pos = nltk.pos_tag(words)
+    for w in pos:
+        if w[1][0] in allowed_word_types:
+            all_words.append(w[0].lower())
+
+    
+for p in short_neg.split('\n'):
+    documents.append( (p, "1") )
+    words = word_tokenize(p)
+    pos = nltk.pos_tag(words)
+    for w in pos:
+        if w[1][0] in allowed_word_types:
+            all_words.append(w[0].lower())
+
+
+
+documents_temp = open("/Users/jk/Documents/Major/plugin_Test/documents.pickle", "rb")
+documents = pickle.load(documents_temp)
+documents_temp.close()
+
+
+
+word_features_temp = open("/Users/jk/Documents/Major/plugin_Test/word_features5k.pickle", "rb")
+word_features = pickle.load(word_features_temp)
+word_features_temp.close()
+
+
+
+def find_features(document):
+    words = word_tokenize(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+
+feature_sets_temp = open("/Users/jk/Documents/Major/plugin_Test/features.pickle", "rb")
+feature_sets = pickle.load(feature_sets_temp)
+feature_sets_temp.close()
+
+
+
+random.shuffle(feature_sets)
+
+
+training_set = feature_sets[:10000]
+testing_set = feature_sets[10000:]
+
+
+open_file = open("/Users/jk/Documents/Major/plugin_Test/originalnaivebayes5k.pickle", "rb")
+classifier = pickle.load(open_file)
+open_file.close()
+
+
+
+voted_classifier = VoteClassifier(classifier)
+                                 
+
+def senti(text_doc):
+    feats = find_features(text_doc)
+    return voted_classifier.classify(feats)
+
+
+
+
+
+###############################################
+
 @csrf_exempt
 def func_hello(request):
 	''' This function is just for testing purposes '''
@@ -80,13 +232,56 @@ def plugin(request):
 		    else:
 		    	val2.append(0)
 
-		exx["val"] = val
-		exx["val2"] = val2
-		exx["links"] = resp1
+		# exx["val"] = val
+		# exx["val2"] = val2
+		# exx["links"] = resp1
 
-		ll = len(val)
+		sentimnt = []
+		for i in range(len(resp)):
+			try:
+				html=urllib.request.urlopen(resp[i])
+				abcd = text_from_html(html)
+				sentimnt.append(eval(senti(abcd)))
+
+			except Exception as e:
+				sentimnt.append(0)
+
+		###############################################
+		urls=pd.read_csv('/Users/jk/Documents/Major/plugin_Test/url.txt',delimiter=',')
+		allurldata=pd.DataFrame(urls)
+		allurldata=np.array(allurldata)
+		random.shuffle(allurldata)
+
+
+		y=[d[1] for d in allurldata]
+		corpus=[d[0] for d in allurldata]
+		vect_url= TfidfVectorizer(tokenizer=convert_to_tokens)
+		x=vect_url.fit_transform(corpus)
+
+
+		x_train ,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+
+		value=LogisticRegression()
+		value.fit(x_train,y_train)
+
+		malicious = []
+		for i in range(len(resp)):
+			try:
+				x_predict=[resp[i]]
+				x_predict=vect_url.transform(x_predict)
+				y_predict=value.predict(x_predict)
+				str1 = ''.join(map(str,(y_predict)))
+				# print(str1)
+				malicious.append(eval(str1))
+			except Exception as e:
+				return HttpResponse(e)
+
+##########################################################
+
+		ll = len(resp)
 		cc = 0
-		ss = [[0 for i in range(5)] for j in range(ll)]
+		ss = [[0 for i in range(7)] for j in range(ll)]
 		for i in range(ll):
 			if(val[i] == 1):
 				ss[i][0] = str(0)
@@ -108,15 +303,28 @@ def plugin(request):
 				ss[i][3] = str(0)
 				ss[i][4] = str(1)
 
-		head = ['Http','Https','High','Med','Low']
+			
+			if(sentimnt[i] == 0):
+				ss[i][5] = str(0)
+			else:
+				ss[i][5] = str(1)
 
-		with open('/Users/jk/Documents/7th Sem/Major/plugin_Test/sample.csv', "w") as csv_file:
+			if(malicious[i] == 0):
+				ss[i][6] = str(0)
+			else:
+				ss[i][6] = str(1)
+
+		head = ['Http','Https','High','Med','Low','Sentimental','Malicious']
+
+
+
+		with open('/Users/jk/Documents/Major/plugin_Test/sample.csv', "w") as csv_file:
 			writer = csv.writer(csv_file)
 			writer.writerow(head)
 			for i in range(ll):
 				writer.writerow((ss[i]))
 
-		df = pd.read_csv('/Users/jk/Documents/7th Sem/Major/plugin_Test/sample.csv')
+		df = pd.read_csv('/Users/jk/Documents/Major/plugin_Test/sample.csv')
 
 		cor_df = df.corr()
 
@@ -134,7 +342,7 @@ def plugin(request):
 			f+=1
 
 		df['Score'] = score
-		df.to_csv('/Users/jk/Documents/7th Sem/Major/plugin_Test/Score.csv')
+		df.to_csv('/Users/jk/Documents/Major/plugin_Test/Score.csv')
 
 		return HttpResponse(json.dumps(sp))
 
